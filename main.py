58b5a3fec913907a49a1930ee5af12e20b8e5bc4
neil liang <neil2000713@gmail.com>
2022-06-18 22:51:26 +0800

    init

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bot import bot
 from plugins.jjc_watcher import initialize
+#import keep_alive
 
 if __name__ == '__main__':
     import json
     with open('./config.json') as f:
         config = json.load(f)
     initialize(config)
+    #keep_alive.keep_alive()
     bot.run(config['token'])
